4738acac940f37fbec90d88f16c8bcd84e1746dd
Jeniffer Borges <jenifferrborges@gmail.com>
2021-12-17 16:40:02 -0300

    commit 9

--- Curso_DevWeb/Fundamentos_Javascript/operadores.js
+++ Curso_DevWeb/Fundamentos_Javascript/operadores.js
@@ -17,3 +17,29 @@ console.log(b)
 
 b%=2 // b = b % 2
 console.log(b)
+
+console.log("Ver: ")
+console.log('01)', '1' == 1)
+console.log('02)', '1' === 1) //exacly the same
+console.log('03)', '3' != 3)
+console.log('04)', '4' !== 3)
+
+
+console.log("Ver2: ")
+function purchases(money1, salary){
+    const buy1 = money1 || salary
+    const buy2 = money1 && salary
+    const buy3 = money1 != salary
+    const notbuy = !buy1
+
+    return {buy1, buy2, buy3, notbuy}
+}
+
+console.log(purchases(false, true))
+
+
+console.log("Ver3: ")
+const result = grade => grade >= 7 ? 'V' : 'F' // if & else
+
+console.log(result(8.4))
+console.log(result(5))
---
+++ Curso_DevWeb/Fundamentos_Javascript/trycatchthrow.js
@@ -0,0 +1,18 @@
+function error(error){
+    throw new Error('cant use toUpperCase')
+}
+
+function shout(obj){
+    try{
+        console.log(obj.name.toUpperCase() + "!!!")
+    }
+    catch(e){
+        error(e)
+    }
+    finally{
+        console.log("End.")
+    }
+}
+
+const obj = {name: 44}
+shout(obj)
