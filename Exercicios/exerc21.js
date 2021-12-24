441ec4a945fb463ab6499c72c35d8cd66e799288
Jeniffer Borges <jenifferrborges@gmail.com>
2021-12-24 16:11:04 -0300

    commit 4 (exerc)

--- Exercicios/exerc21.js
+++ Exercicios/exerc21.js
@@ -3,3 +3,20 @@ considerando que todos pagam R$ 100 mais um adicional conforme a seguinte tabela
 de 10 anos pagam R$80; 2) conveniados com idade entre 10 e 30 anos pagam R$50; 3) conveniados com
 idade acima de 30 e até 60 anos pagam R$ 95; e 4) conveniados acima de 60 anos pagam R$130 */
 
+const health = function (age){
+    let value = 100
+
+    if(age < 10)
+    value += 80
+    if(age >= 10 && age <= 30)
+    value +=50
+    if(age > 30 && age <= 60)
+    value +=95
+    if(age > 60)
+    value += 130
+
+    return value
+}
+
+console.log(health())
+console.log(health(50))
---
+++ Exercicios/exerc25.js
@@ -0,0 +1,8 @@
+/* Escrever um programa para exibir os números de 1 até 50 na tela. */
+
+(function(){
+    for(let i = 1; i < 51; i++){
+        console.log(i)
+    }
+})()
+
